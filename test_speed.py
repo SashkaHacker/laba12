85fc82b0d1be964e8ab3294daa4465e0d2a4504a
SashkaHacker <am942081@gmail.com>
2023-12-02 12:02:39 +0300

    fix all bugs!

--- test_speed.py
+++ test_speed.py
@@ -12,13 +12,13 @@ from hard import binary_search
 from random import randint
 lst = [randint(1, 1000) for i in range({n})]
 lst.append(100)
-lst.sort
+lst.sort()
 el = 100'''
     use = '''
 try:
     binary_search(el, len(lst) - 1, lst, 0)
 except:
-    pass'''
+    print(lst)'''
 
     return timeit.timeit(stmt=use, setup=setup, number=10000)
 
@@ -30,9 +30,11 @@ def create_excel():
     ws.title = "Chart of speed"
 
     # fill values in cell
-    lst = [10, 20, 30, 40, 100, 200, 400, 800, 1000, 1500]
+    lst = [i for i in range(10, 100000, 500)]
+    print(len(lst))
+    # lst = [10, 100, 500, 1000, 2000, 5000, 7000, 10000, 50000, 100000]
     matrix = [['Кол-во элементов:', 'Время:']]
-    for i in range(10):
+    for i in range(200):
         result = round(timer(lst[i]), 6)
         matrix.append([lst[i], result])
     for row in matrix:
@@ -45,11 +47,11 @@ def create_excel():
     chart.x_axis.title = 'Number of values'
     chart.y_axis.title = 'Time'
 
-    data = Reference(ws, min_col=2, min_row=2, max_col=2, max_row=11)
+    data = Reference(ws, min_col=2, min_row=2, max_col=2, max_row=201)
     chart.add_data(data)
-    category = Reference(ws, min_row=2, max_row=11, min_col=1, max_col=1)
+    category = Reference(ws, min_row=2, max_row=201, min_col=1, max_col=1)
     chart.set_categories(category)
-    ws.add_chart(chart, "A15")
+    ws.add_chart(chart, "D1")
 
     wb.save('Chart.xlsx')
 
